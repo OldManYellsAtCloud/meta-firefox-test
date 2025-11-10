f87ff6ac08951f450826f3b0980a6a19518ad641
Gyorgy Sarvari <skandigraun@gmail.com>
2025-11-10 11:34:26 +0100

    update example.com's text

--- recipes-test/firefox-tests/files/smoke/smoke_test.py
+++ recipes-test/firefox-tests/files/smoke/smoke_test.py
@@ -9,7 +9,7 @@ def test_smoke():
     driver = webdriver.Firefox()
     driver.get("https://example.com")
     paragraphs = driver.find_elements(By.TAG_NAME, "p")
-    expected_text = "This domain is for use in illustrative examples in documents. You may use this domain in literature without prior coordination or asking for permission."
+    expected_text = "This domain is for use in documentation examples without needing permission. Avoid use in operations."
     actual_text = paragraphs[0].text
     driver.quit()
     assert actual_text == expected_text, "Could not load example.com. Actual text: %s" % actual_text
