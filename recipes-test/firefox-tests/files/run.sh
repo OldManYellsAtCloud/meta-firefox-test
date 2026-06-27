48c2af632f8bdf7fb005f0be1533de12313fe0f6
Gyorgy Sarvari <skandigraun@gmail.com>
2026-06-27 08:42:13 +0200

    make wayland tests run on scarthgap also
    This is petty ugly...
    So scarthgap uses sysvinit, wrynose and master use systemd.
    With sysvinit the wayland socket is /var/run/user/1000/wayland-1.
    Which is not the default socket name, and the folder is not owned
    
    On master the same socket is in /run/wayland-0.
    
    By default, neither exposes the XDG_RUNTIME_DIR nor WAYLAND_DISPLAY
    environment variables.
    
    So the problem: firefox can't find the wayland socket consistently.
    And when they are found, they need to reside inside a folder that is
    owned by the user running the process.
    
    So, what this change does is it sets the environment variables,
    and takes ownership of the XDD_RUNTIME_DIR, if needed. The last part
    is admittedly ugly - but quick.

--- recipes-test/firefox-tests/files/run.sh
+++ recipes-test/firefox-tests/files/run.sh
@@ -1,22 +1,33 @@
 #!/bin/sh
-export DISPLAY=:0
-export WAYLAND_DISPLAY=/run/wayland-0
 
-# Disable screen blanking and screensaver
-# do this before anything else. Should do no harm,
-# even if the rest fails. This is only for x11.
-# For weston this is done in /etc/xdg/weston/weston.ini
-xset s off
-xset s noblank
-xset -dpms
+if [ $# -ne 1 ]; then                                  
+  echo Missing test folder argument!              
+  exit 1                                          
+fi 
 
-if [ $# -ne 1 ]; then
-  echo Missing test folder argument!
-  exit 1
+# check if we are in wayland of x11
+if id -u weston; then
+  # scarthgap
+  if [ -e /run/wayland-0 ]; then
+    export WAYLAND_DISPLAY=/run/wayland-0
+  elif [ -e /var/run/user/1000/wayland-1 ]; then
+    export WAYLAND_DISPLAY=wayland-1
+    export XDG_RUNTIME_DIR=/var/run/user/1000
+    chown root $XDG_RUNTIME_DIR
+  fi
+else
+  # Disable screen blanking and screensaver              
+  # do this before anything else. Should do no harm,     
+  # even if the rest fails. This is only for x11.        
+  # For weston this is done in /etc/xdg/weston/weston.ini
+  xset s off           
+  xset s noblank                      
+  xset -dpms 
+  export DISPLAY=:0
 fi
 
 # the tests should be in the home folder
 cd /home/root
 
-pytest --junitxml=./$1.xml $1/
 
+pytest --junitxml=./$1.xml $1/
